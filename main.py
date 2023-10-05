ce92b7aaa5c6f6934f8441de1ef17e565837df0f
Artem Golovin <golovin.av@micran.ru>
2023-10-05 23:02:46 +0200

    Add main function using the detector

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from cards.card import Card
-
-
-def main():
-    card = Card()
-    print(card.rank, card.suit)
-
-
+import cv2
+from detector.CardDetector import CardDetector
 
 if __name__ == "__main__":
-    main()
+    ### ---- MAIN LOOP ---- ###
+    # The main loop repeatedly grabs frames from the video stream
+    # and processes them to find and identify playing cards.
+
+    cam_quit = False  # Loop control variable
+    card_detector = CardDetector(video_src=2)
+
+    # Begin capturing frames
+    while not cam_quit:
+
+        card_detector.detect_cards()
+        image, _ = card_detector.last_images()
+
+        cv2.imshow("Card Detector", image)
+
+        # Poll the keyboard. If 'q' is pressed, exit the main loop.
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            cam_quit = True
+
+    del card_detector
