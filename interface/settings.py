bfa18e737bbcbec0bac6d0a0352afbdbcee180ee
nbezhodarnov <nbezhodarnov@gmail.com>
2023-10-15 20:38:33 +0200

    Add microphone choose parameter to settings

--- interface/settings.py
+++ interface/settings.py
@@ -3,3 +3,4 @@ from utils.singletonmeta import SingletonMeta
 class Settings(metaclass=SingletonMeta):
     def __init__(self):
         self.camera_id = 0
+        self.microphone_id = 0
--- interface/settingsmenuview.py
+++ interface/settingsmenuview.py
@@ -6,6 +6,10 @@ import arcade.gui
 
 import cv2
 
+import speech_recognition as sr
+
+import locale
+
 class SettingsView(arcade.View):
     def __init__(self):
         super().__init__()
@@ -31,6 +35,21 @@ class SettingsView(arcade.View):
         def on_change_dropdown(event):
             Settings().camera_id = int(event.new_value)
 
+        microphones_list_label = arcade.gui.UILabel(
+            font_size=18,
+            align="center",
+            text="Select microphone:",
+        )
+
+        microphones_list = arcade.gui.UIDropdown(
+            options=self.__list_available_microphones(),
+            width=200,
+            height=40,)
+        
+        @microphones_list.event("on_change")
+        def on_change_dropdown(event):
+            Settings().microphone_id = self.__list_available_microphones().index(event.new_value)
+
         back_to_menu_button = arcade.gui.UIFlatButton(
             width=200,
             height=40,
@@ -45,6 +64,8 @@ class SettingsView(arcade.View):
         vertical_box = arcade.gui.UIBoxLayout()
         vertical_box.add(cameras_list_label)
         vertical_box.add(cameras_list)
+        vertical_box.add(microphones_list_label)
+        vertical_box.add(microphones_list)
         vertical_box.add(back_to_menu_button)
 
         layout = self.ui_manager.add(arcade.gui.UIAnchorLayout())
@@ -72,3 +93,11 @@ class SettingsView(arcade.View):
                 break
         
         return available_cameras
+    
+    def __list_available_microphones(self):
+        microphone_list = sr.Microphone.list_microphone_names()
+        for i in range(len(microphone_list)):
+            # Function 'list_microphone_names' return strings encoded in system preferred encoding.
+            # Python's default encoding is utf-8, so we need to convert it to utf-8.
+            microphone_list[i] = bytes(microphone_list[i], locale.getpreferredencoding()).decode("utf-8")
+        return microphone_list
